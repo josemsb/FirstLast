589234cbe5812782da98db84fb20fbf34428c66d
josemsb <josemsb@gmail.com>
2026-05-04 15:35:59 -0500

    fix: remove duplicate MaterialTheme import in OnboardingScreen

--- app/src/main/java/com/appgrouplab/firstlast/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/appgrouplab/firstlast/presentation/OnboardingScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
